92c8492313d25b0619e65f56538e16a25fa4d5c2
renierr <renierr@users.noreply.github.com>
2025-12-06 23:43:14 +0100

    fix tailwind dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,4 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',
-  content: ['./index.html', './src/**/*.{html,ts}'],
-  theme: { extend: {} },
-  plugins: [],
+  content: ['./index.html', './src/**/*.{html,js,ts,jsx,tsx}'],
 };
